6aa22a0d2921dea49113a2bd3d8ee8fe66132a24
任博恩 <308528611@qq.com>
2018-03-09 15:10:30 +0800

    利用vw制作vuewebapp多端适配

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>main</h1>
+    <div class="title">uuu</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods:{}
+}
+</script>
+
+<style>
+*{
+  margin: 0;padding: 0;
+}
+:root{
+  --cp:blue;
+}
+  :global .title {
+ font-size: 100px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: var(--cp)
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1>{{msg}}<br/>{{change}}<br/>{{shezhi}} 
+   <span>oooo</span>
+   <div class="title">mmmm</div>
+    <br/></h1>
+    <div>
+    <button @click.self="add">clck</button>
+    </div>
+ 
+    <img src="../assets/logo.png" >
+
+<mt-button
+    type="primary"
+    @click="sheetVisible = true">
+    选择操作
+  </mt-button>
+
+  </div>
+</template>
+
+<script>
+import { Button } from 'mint-ui';
+  export default {
+    name: 'moduleNmame',
+    data() {
+      return {
+        msg: "msk"
+      }
+    },
+    computed: {
+      change() {
+        return this.msg + "change"
+      },
+      shezhi:{
+        set(){
+          return this.msg+"set"
+        },
+        get(){
+          return this.msg+"get"
+        }
+      }
+    },
+    methods: {
+      add(){
+        this.msg="hk";
+      
+      }
+    }
+  }
+</script>
+
+<style scoped >
+:root{
+  --cp:red;
+  --wd:750px
+}
+  h1{
+    width: var(--wd);
+    height: 400px;
+    border: 1px solid dashed;
+    box-sizing: border-box;
+    background: var(--cp);
+    font-size: 50px;
+    color:yellow;
+   @nest & span{
+    color: green;
+  }
+  }
+
+
+</style>
+
+
